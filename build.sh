4093c4394a6307a52b01e552029660ad24c05279
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-05-24 03:53:16 -0300

    Update jupyterhub files

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 set -e
-
-# docker network create BDC-JUPYTER-NETWORK
-
 docker-compose rm -f && docker-compose build && docker-compose up -d
--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -1,37 +1,38 @@
+# JupyterHub configuration
 import os
 
-c = get_config()
-
-c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
-c.DockerSpawner.container_image = os.environ['DOCKER_NOTEBOOK_IMAGE']
-
-# Networking
-network_name = os.environ['DOCKER_NETWORK_NAME']
-c.DockerSpawner.use_internal_ip = True
-c.DockerSpawner.network_name = network_name
-c.DockerSpawner.extra_host_config = { 'network_mode': network_name }
-
-## User containers will access hub by container name on the Docker network
-c.JupyterHub.hub_ip = 'bdc-jupyterhub'
-c.JupyterHub.hub_port = 8080
-
-# c.JupyterHub.services = [
-#     {
-#         'name': 'cull_idle',
-#         'admin': True,
-#         'command': 'python /srv/jupyterhub/cull_idle_servers.py --timeout=3600'.split(),
-#     },
-# ]
-
-# mapping rules
-# c.Spawner.default_url = '/bdc/lab'
-
-# auth
+## Authenticator
 # https://oauthenticator.readthedocs.io/en/latest/getting-started.html#gitlab-setup
 # http://tljh.jupyter.org/en/latest/howto/auth/google.html
 from oauthenticator.google import GoogleOAuthenticator
 c.JupyterHub.authenticator_class = GoogleOAuthenticator
 
-c.JupyterHub.authenticator_class.client_id = "61086726892-0lqd6s4lka472ct2aqljb5fsgrdpsv6s.apps.googleusercontent.com"
-c.JupyterHub.authenticator_class.client_secret = "o-O0tJunve8wKr3P7bw_8L6O"
-c.JupyterHub.authenticator_class.oauth_callback_url = "http://localhost:8000/hub/oauth_callback"
+c.JupyterHub.authenticator_class.client_id = os.environ['GOOGLE_CLIENTID']
+c.JupyterHub.authenticator_class.client_secret = os.environ['GOOGLE_CLIENT_SECRET']
+c.JupyterHub.authenticator_class.oauth_callback_url = os.environ['GOOGLE_OAUTH_CALLBACK_URL']
+
+## Docker spawner
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
+c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+# See https://github.com/jupyterhub/dockerspawner/blob/master/examples/oauth/jupyterhub_config.py
+c.JupyterHub.hub_ip = os.environ['HUB_IP']
+
+## user data persistence
+## see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
+notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan'
+c.DockerSpawner.notebook_dir = notebook_dir
+c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
+
+# Server usage
+c.Spawner.cpu_limit = 1
+c.Spawner.mem_limit = '4G'
+
+## Services
+c.JupyterHub.services = [
+    {
+        'name': 'cull_idle',
+        'admin': True,
+        'command': 'python /srv/jupyterhub/cull_idle_servers.py --timeout=3600'.split(),
+    },
+]
